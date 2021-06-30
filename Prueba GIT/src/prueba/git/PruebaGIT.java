3bb08112c672e68069f41a963bdb22d1c2f5acf8
David Cattáneo <davidcattaneo@yahoo.com.ar>
2021-06-30 20:30:18 -0300

    Prueba branches

--- Prueba GIT/src/prueba/git/PruebaGIT.java
+++ Prueba GIT/src/prueba/git/PruebaGIT.java
@@ -10,6 +10,8 @@ public class PruebaGIT {
 	
 	public static void main(String[] args) {
 		//Haremos la prueba del tercer Commit
+		//Ahora veremos el primer Commit en la branch rama1 para probar si sube este cambio al repositorio de rama
+		
 	}
 	
 }
